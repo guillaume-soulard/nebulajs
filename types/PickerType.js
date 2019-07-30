1b2f2300db9cbf0fde9f940c6d2a6432559e453a
ogama <ogama47500@gmail.com>
2019-07-30 15:27:31 +0200

    Fix picker type index calculation

--- types/PickerType.js
+++ types/PickerType.js
@@ -11,7 +11,7 @@ exports.PickerType = extend(true, {}, AbstractType, {
             throw new Error('items option must be defined');
         }
 
-        let index = parseInt((Math.random() * context.options.items.length)
+        let index = parseInt((Math.random() * (context.options.items.length - 1))
             .toFixed(0));
 
         return context.options.items[index];
