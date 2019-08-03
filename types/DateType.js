afaf0582e0c0959906068d879bbeb7dfb9cf8f84
ogama <ogama47500@gmail.com>
2019-08-03 22:19:50 +0200

    Adding date tuncate

--- types/DateType.js
+++ types/DateType.js
@@ -1,12 +1,23 @@
 const extend = require('extend');
 const AbstractType = require('./AbstractType').AbstractType;
 
+const truncateList = {
+    "milliseconds": 0,
+    "seconds": 1,
+    "minutes": 2,
+    "hours": 3,
+    "days": 4,
+    "months": 5,
+    "years": 6
+};
+
 exports.DateType = extend(true, {}, AbstractType, {
     options: {
         bounds: {
             min: 0,
             max: Number.MAX_VALUE
-        }
+        },
+        truncate: "milliseconds"
     },
     generate: (context) => {
 
@@ -28,7 +39,38 @@ exports.DateType = extend(true, {}, AbstractType, {
             maxTimestamp = new Date(context.options.bounds.max).getTime();
         }
 
-        return new Date(parseInt((Math.random() * (maxTimestamp - minTimestamp) + minTimestamp)
-            .toFixed(0)));
+        return truncateDate(new Date(parseInt((Math.random() * (maxTimestamp - minTimestamp) + minTimestamp)
+            .toFixed(0))), context.options.truncate);
     }
 });
+
+function truncateDate(date, truncate) {
+
+    let truncatePart = truncateList[truncate];
+
+    if (truncatePart >= 6) {
+        date.setUTCMonth(0);
+    }
+
+    if (truncatePart >= 5) {
+        date.setUTCDate(1);
+    }
+
+    if (truncatePart >= 4) {
+        date.setUTCHours(0);
+    }
+
+    if (truncatePart >= 3) {
+        date.setUTCMinutes(0);
+    }
+
+    if (truncatePart >= 2) {
+        date.setUTCSeconds(0);
+    }
+
+    if (truncatePart >= 1) {
+        date.setUTCMilliseconds(0);
+    }
+
+    return date;
+}
