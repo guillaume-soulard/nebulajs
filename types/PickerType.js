f8409058e24686e40fc36797beac7ce485fd4d88
ogama <ogama47500@gmail.com>
2019-08-07 20:43:00 +0200

    Adding probabilities in picker

--- types/PickerType.js
+++ types/PickerType.js
@@ -8,9 +8,63 @@ exports.PickerType = {
             throw new Error('items option must be defined');
         }
 
-        let index = parseInt((Math.random() * (context.options.items.length - 1))
-            .toFixed(0));
+        if (isCorrectProbabilisticItems(context.options.items)) {
 
-        return context.options.items[index];
+            let object;
+            let items = [];
+            let offset = 0;
+
+            for (let i = 0; i < context.options.items.length; i++) {
+                items.push({
+                    value: context.options.items[i].value,
+                    min: offset,
+                    max: context.options.items[i].probability + offset
+                });
+
+                offset += context.options.items[i].probability;
+            }
+
+            let random = parseFloat((Math.random() * (100 - 1)));
+
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].min <= random && items[i].max > random) {
+                    object = items[i].value;
+                    break;
+                }
+            }
+
+            return object;
+        } else {
+            let index = parseInt((Math.random() * (context.options.items.length - 1))
+                .toFixed(0));
+
+            return context.options.items[index];
+        }
     }
 };
+
+function isCorrectProbabilisticItems(items) {
+
+    let probabilisticItemsCount = 0;
+    let nonProbabilisticItemsCount = 0;
+    let probabilitySum = 0;
+
+    for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+        if (items[itemIndex].value !== undefined && items[itemIndex].probability !== undefined) {
+            probabilisticItemsCount++;
+            probabilitySum += items[itemIndex].probability;
+        } else {
+            nonProbabilisticItemsCount++;
+        }
+    }
+
+    if (probabilisticItemsCount > 0 && nonProbabilisticItemsCount > 0) {
+        throw new Error('Probabilistic items structure is incorrect');
+    }
+
+    if (probabilitySum !== 100) {
+        throw new Error('Probability sum is not equals to 100');
+    }
+
+    return probabilisticItemsCount > 0;
+}
